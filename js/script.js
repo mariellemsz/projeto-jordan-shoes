cc647183046b5dbe17a776cb0d5930c96f19ce45
Marielle <marielle.msilva2004@gmail.com>
2026-02-06 15:09:34 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+console.log('Jordan Shoes')
+
+const getProducts = async () => {
+    const response = await fetch('js/products.json')
+    const data = await response.json()
+    return data
+}
+
+const generateCard = async () => {
+    
+    const products = await getProducts()
+
+    products.map(product => {
+
+        let card = document.createElement('div')
+        card.classList.add('card__produto')
+        console.log(card)
+
+        card.innerHTML = `
+        <figure>
+            <img src="images/${product.image}" alt="${product.product_name}">
+        </figure>
+
+        <div class="card__produto__detalhes">
+            <h4>${product.product_name}</h4>
+            <h5>${product.product_model}</h5>
+            </div>
+
+        <h6> R$ ${product.price}</h6>
+        `
+        const listaProdutos = document.querySelector('.lista__produtos')
+        listaProdutos.appendChild(card)
+
+    })
+}
+
+generateCard()
+
